b38a91b76be592d3467ae358476e87a5f5523faa
antbucc <bucchiarone@fbk.eu>
2022-08-29 22:47:37 +0200

    response API added

--- server.js
+++ server.js
@@ -10,17 +10,19 @@ const HOST = '0.0.0.0';
 const app = express();
 app.get('/', (req, res) => {
     console.log("eccolo");
+    res.json('{ success: true }')
+
 
 });
 app.post('/', (req, res) => {
 
-    console.log("here");
+
     // wsynth.exe -model_type dn -out_type dot -algo agaf_then_acyclic_preferences -agaf states -mono -dynamic -reachability_analysis file.smv > .\output.dot
 
     var execFile = require('child_process').execFile;
 
 
-    execFile('wine wsynth.exe', ['-model_type', 'dn', '-out_type', 'dot', '-algo', 'agaf_then_acyclic_preferences', '-agaf', 'states', '-mono', '-dynamic', '-reachability_analysis', './test.smv'], function (err, data) {
+    execFile('wsynth.exe', ['-model_type', 'dn', '-out_type', 'dot', '-algo', 'agaf_then_acyclic_preferences', '-agaf', 'states', '-mono', '-dynamic', '-reachability_analysis', './test.smv'], function (err, data) {
         if (err) {
             console.log("ERRORE: " + err);
         }
